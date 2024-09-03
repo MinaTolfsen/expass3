be4630a69b9fb41a67db94b02c855110ce904660
Mina Tolfsen <minabye@me.com>
2024-09-03 11:47:15 +0200

    Init

---
+++ src/main/java/com/example/expass/ExpassApplication.java
@@ -0,0 +1,13 @@
+package com.example.expass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expass/ExpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
